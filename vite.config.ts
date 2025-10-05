b3af081143a70b8b868ed8ae4b90db78746d0a8f
toeicphat <toeicphat@gmail.com>
2025-10-05 15:08:44 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ base: './', }) => {
+export default defineConfig({
+  base: '/TOEIC-ZONE/', 
+  plugins: [react()],
+}) => {
     const env = loadEnv(mode, '.', '');
     return {
       server: {
